fea6eca6c3e8a9a31a1ebe4b072b8689c4791494
AtomskX <rlamezquita@gmail.com>
2019-05-29 18:30:05 -0400

    Working Base
    GUI working. Stack is using a string array.

---
+++ PRJ1Amezquita.java
@@ -0,0 +1,188 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package PRJ1AmezquitaR;
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import java.lang.*;
+
+
+public class PRJ1AmezquitaR {
+      
+    public static class GUI extends JFrame implements ActionListener{
+        
+        //create labels
+        private JLabel expression, result;
+        
+        //create input and output box
+        private JTextField input, output;
+        
+        //create button
+        private JButton evaluate;
+        
+        //create panels
+        private JPanel dataIn, button, dataOut;
+        
+        //variable for entered data
+        private String enteredExpression;
+        
+        //variables for stacks
+        private ArrayStack operators, operands;
+                
+        //this is the GUI constructor
+        public GUI(){
+            
+            //initialize panel for the input field and textbox
+            dataIn = new JPanel();
+            button = new JPanel();
+            dataOut = new JPanel();
+                                   
+            //initialze input text field and label
+            expression = new JLabel("Enter Infix Expression");
+            input = new JTextField("Ex. 4 + 3");
+            
+            //set the size of the input box
+            input.setPreferredSize(new Dimension(200,20));
+            
+            //initialize button
+            evaluate = new JButton("Evaluate");
+            evaluate.setPreferredSize(new Dimension(100,30));
+            
+            //initialize the output text field and label
+            output = new JTextField();
+            result = new JLabel("Result");
+            output.setEditable(false);
+            
+            //set the size of the output box
+            output.setPreferredSize(new Dimension(200,20));
+            
+            //setting borders
+            dataIn.setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
+            dataIn.setLayout(new FlowLayout(FlowLayout.CENTER));
+            
+            //setting borders
+            button.setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
+            button.setLayout(new FlowLayout(FlowLayout.CENTER));
+            
+            //setting borders
+            dataOut.setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
+            dataOut.setLayout(new FlowLayout(FlowLayout.CENTER));
+            
+            //add label and field to the dataIn panel
+            dataIn.add(expression);
+            dataIn.add(input);
+            
+            //add button to button panel
+            button.add(evaluate);
+            
+            //add label and output box to dataOutPanel
+            dataOut.add(result);
+            dataOut.add(output);
+            
+            //add panels to the frame
+            this.add(dataIn, BorderLayout.NORTH);
+            this.add(button, BorderLayout.CENTER);
+            this.add(dataOut, BorderLayout.SOUTH);
+            
+            //add actionlistener to button
+            evaluate.addActionListener(this);
+      
+            
+        }//end GUI constructor
+        
+        //button click actions
+        @Override
+        public void actionPerformed(ActionEvent e){
+            
+            //tokenize input
+            char[] tokens = input.getText().toCharArray();
+  
+            
+            System.out.print(tokens[1]);
+            
+            
+            output.setText(String.valueOf(tokens[0]));
+            
+            
+        }// end actionPerformed
+        
+        
+    }//end GUI class
+    
+    //stack interface
+    public interface Stack {
+        public boolean isFull();
+        public boolean isEmpty(); 
+        public String peek();
+        public void push(String item);
+        public String pop();
+    
+    }//end stack
+    
+    //class for constructing stack
+    public static class ArrayStack implements Stack{
+        
+        //variable for default size of stack
+        private static final int defaultSize = 10;
+        
+        //initialize array
+        String elements[];
+        
+        //variable for how many elements are in the array
+        private int numberOfElements = 0;
+        
+        //constructor for ArrayStack
+        public ArrayStack(){
+                      
+           elements = new String[defaultSize];
+                   
+        }// end ArrayStack()
+        
+        public boolean isFull(){
+            
+            //place holder
+            return true;
+            
+        }// end isFull
+        public boolean isEmpty(){
+            
+            //place holder
+            return true;
+            
+        }// end isEmpty
+        public String peek(){
+            
+            //place holder
+            return "test";            
+            
+        }// end peek
+        public void push(String item){
+            
+        }// end push
+        public String pop(){
+            
+            //place holder
+            return "test";
+            
+        }// end pop
+        
+        
+    }//end ArrayStack
+    
+    public static void main(String[] args) {
+        
+        GUI solver = new GUI();
+        
+        solver.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        solver.setLocationRelativeTo(null);
+        solver.setVisible(true);
+        solver.setSize(450, 175);
+        solver.setTitle("Infix Expression Evaluator");
+     
+    }//end main class
+    
+}//end PRJ1Amezquita
